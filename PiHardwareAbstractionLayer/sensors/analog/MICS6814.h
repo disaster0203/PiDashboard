e682dde3493a61af22f59eef261cc06b5160d04c
MartinR <martin.ruehlicke@gmail.com>
2020-04-07 22:37:20 +0200

    Add mics6814 (analog co/nh3/no2 sensor) class

---
+++ PiHardwareAbstractionLayer/sensors/analog/MICS6814.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <mutex>
+#include <cmath>
+
+#include "../../enums/SensorSetting.h"
+#include "../../interfaces/ISensor.h"
+#include "../../utils/EnumConverter.h"
+#include "../../utils/Helper.h"
+
+using namespace hal::utils;
+
+namespace hal
+{
+	namespace sensors
+	{
+		namespace analog
+		{
+			namespace mics6814
+			{
+
+			}
+		}
+	}
+}
